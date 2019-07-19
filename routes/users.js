ab61262ebaf7d98b4d93ec90518b4331d52d6866
Jegan Matthews <jematthe@starbucks.com>
2019-07-18 23:58:11 -0700

    Why not get

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("why not?");
 });
 
-router.post('/', function(req, res, next){
-	res.send({
-		id: 1,
-		msg: "success"
-	})
+router.post("/", function(req, res, next) {
+  res.send({
+    id: 1,
+    msg: "success"
+  });
 });
 module.exports = router;
